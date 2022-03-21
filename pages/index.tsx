cf400a01c4f7a59985f2a4b6ea153db119705705
Sushant Garudkar <garudkarsushant7@gmail.com>
2022-03-22 01:02:55 +0530

    Add Background Image

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,18 +1,21 @@
+import Header from '@/components/Header'
 import type { NextPage } from 'next'
 import Head from 'next/head'
 
 const Home: NextPage = () => {
   return (
-    <div className="">
+    <div
+      style={{ backgroundImage: "url('images/background/background.png')" }}
+      className="min-h-screen bg-repeat-y"
+    >
       <Head>
         <title>E - Bazaar</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <h1>
-        Hare Krisha Hare Krishna Krishna Krishna Hare Hare Hare Ram Hare Ram Ram
-        Ram Hare Hare
-      </h1>
+      <div className="min-h-screen backdrop-blur-3xl">
+        <Header />
+      </div>
     </div>
   )
 }

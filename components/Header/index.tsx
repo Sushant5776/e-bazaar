b4d80e52828672cc1702cf2f847635a520131de9
Sushant Garudkar <garudkarsushant7@gmail.com>
2022-03-22 01:32:11 +0530

    Add Header Component

--- components/Header/index.tsx
+++ components/Header/index.tsx
@@ -1,8 +1,19 @@
+import { ShoppingBagIcon, DotsHorizontalIcon } from '@heroicons/react/outline'
+
 const Header = () => {
   return (
-    <div>
-      
-    </div>
+    <header className="flex items-center justify-between">
+      {/* Left */}
+      <div className="flex items-start space-x-2 rounded-tr-[48px] rounded-br-[18px] bg-[#373c370d] py-1 px-5">
+        <ShoppingBagIcon className="h-7 w-7" />
+        <h1 className="text-2xl font-medium text-[#FFBF4E]">E-Bazaar</h1>
+      </div>
+
+      {/* Right */}
+      <div className="rounded-tl-[18px] rounded-bl-[48px] bg-[#373c370d] py-[6px] px-4">
+        <DotsHorizontalIcon className="relative left-1 h-7 w-7" />
+      </div>
+    </header>
   )
 }
 
